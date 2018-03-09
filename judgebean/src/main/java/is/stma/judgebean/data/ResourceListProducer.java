nstickney/CyBearPit

commit a413c5faaaa5c184cad016dfeac00ebff8748dd8

add: basic website tabs/panels layout

judgebean/pom.xml
@@ -38,6 +38,7 @@
     <properties>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
         <java.version>1.8</java.version>
+        <jboss-bom.version>8.2.2.Final</jboss-bom.version>
         <deltaspike.version>1.8.1</deltaspike.version>
         <maven-war-plugin.version>3.2.0</maven-war-plugin.version>
         <maven.compiler.target>${java.version}</maven.compiler.target>
@@ -56,14 +57,21 @@
             <dependency>
                 <groupId>org.wildfly.bom</groupId>
                 <artifactId>jboss-javaee-7.0-with-tools</artifactId>
-                <version>8.2.2.Final</version>
+                <version>${jboss-bom.version}</version>
                 <type>pom</type>
                 <scope>import</scope>
             </dependency>
             <dependency>
                 <groupId>org.wildfly.bom</groupId>
                 <artifactId>jboss-javaee-7.0-with-hibernate</artifactId>
-                <version>8.2.2.Final</version>
+                <version>${jboss-bom.version}</version>
+                <type>pom</type>
+                <scope>import</scope>
+            </dependency>
+            <dependency>
+                <groupId>org.jboss.spec</groupId>
+                <artifactId>jboss-javaee-7.0</artifactId>
+                <version>RELEASE</version>
                 <type>pom</type>
                 <scope>import</scope>
             </dependency>

judgebean/src/main/java/is/stma/judgebean/data/ResourceListProducer.java
@@ -23,6 +23,12 @@ public class ResourceListProducer extends AbstractEntityListProducer<Resource> {
         return entities;
     }
 
+    @Produces
+    @Named("unassignedResources")
+    public List<Resource> findUnassignedResources() {
+        return repo.findUnassigned();
+    }
+
     @Override
     @PostConstruct
     void retrieveAll() {

judgebean/src/main/java/is/stma/judgebean/data/ResourceRepo.java
@@ -1,10 +1,18 @@
 package is.stma.judgebean.data;
 
 import is.stma.judgebean.model.Resource;
+import org.apache.deltaspike.data.api.Query;
 import org.apache.deltaspike.data.api.Repository;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.List;
 
 @SuppressWarnings("CdiManagedBeanInconsistencyInspection")
 @Repository(forEntity = Resource.class)
 public interface ResourceRepo extends AbstractRepo<Resource> {
+
+    @Query("SELECT r FROM Resource r WHERE r.contest_id IS NULL")
+    List<Resource> findUnassigned();
 
 }

judgebean/src/main/java/is/stma/judgebean/data/TeamListProducer.java
@@ -23,6 +23,12 @@ public class TeamListProducer extends AbstractEntityListProducer<Team> {
         return entities;
     }
 
+    @Produces
+    @Named("unassignedTeams")
+    public List<Team> findUnassignedTeams() {
+        return repo.findUnassigned();
+    }
+
     @Override
     @PostConstruct
     void retrieveAll() {

judgebean/src/main/java/is/stma/judgebean/data/TeamRepo.java
@@ -1,9 +1,12 @@
 package is.stma.judgebean.data;
 
+import is.stma.judgebean.model.Resource;
 import is.stma.judgebean.model.Team;
 import org.apache.deltaspike.data.api.Query;
 import org.apache.deltaspike.data.api.Repository;
 
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
 import java.util.List;
 
 @SuppressWarnings("CdiManagedBeanInconsistencyInspection")
@@ -12,5 +15,8 @@ public interface TeamRepo extends AbstractRepo<Team> {
 
     @Query("select t from Team t where t.contest.id = ?1")
     List<Team> findByContest(String contestId);
+
+    @Query("SELECT t FROM Team t WHERE t.contest_id IS NULL")
+    List<Team> findUnassigned();
 
 }

judgebean/src/main/resources/import.sql
@@ -18,4 +18,26 @@
 --
 
 -- You can use this file to load seed data into the database using SQL statements
-insert into Team (id, name, flag) values (0, 'Blue Man Group', 'Ready, Go!')
+insert into Contest (id, name) values ('C0', 'Cyber Patriot')
+insert into Contest (id, name) values ('C1', 'CCDC')
+insert into Contest (id, name) values ('C2', 'CDX')
+insert into Team (id, name, flag, contest_id) values ('T0', 'Blue Man Group', 'Ready, Go!', 'C0')
+insert into Team (id, name, flag, contest_id) values ('T1', 'Bala Morgab', 'BMG', 'C0')
+insert into Team (id, name, flag, contest_id) values ('T2', 'General Motors Bicyclists', 'GMb', 'C1')
+insert into Team (id, name, flag, contest_id) values ('T3', 'Greater Midwest Baseball', 'GMB', 'C1')
+insert into Team (id, name, flag, contest_id) values ('T4', 'Totally Unrelated People', '123456', 'C2')
+insert into Team (id, name, flag, contest_id) values ('T5', 'Totally Unrelated People', '123456', 'C2')
+insert into Team (id, name, flag, contest_id) values ('T6', 'Totally Unrelated People', '123456', 'C0')
+insert into Team (id, name, flag) values ('T7', 'Totally Unrelated People', '123456')
+insert into Resource (id, name, contest_id) values ('R0', 'T0 DNS', 'C0')
+insert into Resource (id, name, contest_id) values ('R1', 'T1 DNS', 'C0')
+insert into Resource (id, name, contest_id) values ('R2', 'T0 HTTP', 'C0')
+insert into Resource (id, name, contest_id) values ('R3', 'T1 HTTP', 'C0')
+insert into Resource (id, name, contest_id) values ('R4', 'T2 DNS', 'C1')
+insert into Resource (id, name, contest_id) values ('R5', 'T3 DNS', 'C1')
+insert into Resource (id, name, contest_id) values ('R6', 'T2 HTTP', 'C1')
+insert into Resource (id, name, contest_id) values ('R7', 'T3 HTTP', 'C1')
+insert into Resource (id, name, contest_id) values ('R8', 'T4 DNS', 'C2')
+insert into Resource (id, name, contest_id) values ('R9', 'T5 DNS', 'C2')
+insert into Resource (id, name, contest_id) values ('R10', 'T4 HTTP', 'C2')
+insert into Resource (id, name, contest_id) values ('R11', 'T5 HTTP', 'C2')

judgebean/src/main/webapp/resources.xhtml
@@ -1,0 +1,16 @@
+<ui:fragment xmlns="http://www.w3.org/1999/xhtml"
+             xmlns:ui="http://java.sun.com/jsf/facelets"
+             xmlns:b="http://bootsfaces.net/ui">
+
+    <!--Show the unassigned resources first-->
+    <ui:repeat var="_resource" value="#{unassignedResources}">
+        <b:panel look="warning" col-lg="6" title="#{_resource.name}" collapsed="#{true}"/>
+    </ui:repeat>
+
+    <!--The remaining resources are grouped by contest-->
+    <ui:repeat value="#{contests}" var="_contest">
+        <ui:repeat var="_resource" value="#{_contest.resources}">
+            <b:panel col-lg="6" title="#{_contest.name}: #{_resource.name}" collapsed="#{true}"/>
+        </ui:repeat>
+    </ui:repeat>
+</ui:fragment>

judgebean/src/main/webapp/teams.xhtml
@@ -1,0 +1,10 @@
+<ui:fragment xmlns="http://www.w3.org/1999/xhtml"
+             xmlns:ui="http://java.sun.com/jsf/facelets"
+             xmlns:b="http://bootsfaces.net/ui">
+
+    <ui:repeat var="_team" value="#{teams}">
+        <b:panel col-lg="6" title="#{_team.name}" collapsed="#{true}">
+            Flag: #{_team.flag}
+        </b:panel>
+    </ui:repeat>
+</ui:fragment>

commit fd5a04d6cf2df9e799fbe2d5bfa63a33eb33d4e0

fix: 'Internal Server Error' introduced in a413c5f resolved

judgebean/pom.xml
@@ -69,13 +69,6 @@
                 <scope>import</scope>
             </dependency>
             <dependency>
-                <groupId>org.jboss.spec</groupId>
-                <artifactId>jboss-javaee-7.0</artifactId>
-                <version>RELEASE</version>
-                <type>pom</type>
-                <scope>import</scope>
-            </dependency>
-            <dependency>
                 <groupId>org.apache.deltaspike.distribution</groupId>
                 <artifactId>distributions-bom</artifactId>
                 <version>${deltaspike.version}</version>
@@ -220,14 +213,6 @@
             <groupId>dnsjava</groupId>
             <artifactId>dnsjava</artifactId>
             <version>2.1.8</version>
-        </dependency>
-
-        <!-- Java Server Faces -->
-        <dependency>
-            <groupId>org.jboss.spec.javax.faces</groupId>
-            <artifactId>jboss-jsf-api_2.2_spec</artifactId>
-            <version>2.2.14</version>
-            <scope>provided</scope>
         </dependency>
 
         <!-- BootFaces! -->

judgebean/src/main/java/is/stma/judgebean/data/ResourceListProducer.java
@@ -7,6 +7,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestScoped
@@ -23,15 +24,21 @@ public class ResourceListProducer extends AbstractEntityListProducer<Resource> {
         return entities;
     }
 
-    @Produces
-    @Named("unassignedResources")
-    public List<Resource> findUnassignedResources() {
-        return repo.findUnassigned();
-    }
-
     @Override
     @PostConstruct
     void retrieveAll() {
         entities = repo.findAllOrderByNameAsc();
+    }
+
+    @Produces
+    @Named("unassignedResources")
+    public List<Resource> findUnassigned() {
+        List<Resource> unassigned = new ArrayList<>();
+        for (Resource entity : entities) {
+            if (entity.getContest() == null) {
+                unassigned.add(entity);
+            }
+        }
+        return unassigned;
     }
 }

judgebean/src/main/java/is/stma/judgebean/data/ResourceRepo.java
@@ -12,7 +12,4 @@ import java.util.List;
 @Repository(forEntity = Resource.class)
 public interface ResourceRepo extends AbstractRepo<Resource> {
 
-    @Query("SELECT r FROM Resource r WHERE r.contest_id IS NULL")
-    List<Resource> findUnassigned();
-
 }

judgebean/src/main/java/is/stma/judgebean/data/TeamListProducer.java
@@ -1,5 +1,6 @@
 package is.stma.judgebean.data;
 
+import is.stma.judgebean.model.Resource;
 import is.stma.judgebean.model.Team;
 
 import javax.annotation.PostConstruct;
@@ -7,6 +8,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestScoped
@@ -23,15 +25,21 @@ public class TeamListProducer extends AbstractEntityListProducer<Team> {
         return entities;
     }
 
-    @Produces
-    @Named("unassignedTeams")
-    public List<Team> findUnassignedTeams() {
-        return repo.findUnassigned();
-    }
-
     @Override
     @PostConstruct
     void retrieveAll() {
         entities = repo.findAllOrderByNameAsc();
+    }
+
+    @Produces
+    @Named("unassignedTeams")
+    public List<Team> findUnassigned() {
+        List<Team> unassigned = new ArrayList<>();
+        for (Team entity : entities) {
+            if (entity.getContest() == null) {
+                unassigned.add(entity);
+            }
+        }
+        return unassigned;
     }
 }

judgebean/src/main/java/is/stma/judgebean/data/TeamRepo.java
@@ -16,7 +16,4 @@ public interface TeamRepo extends AbstractRepo<Team> {
     @Query("select t from Team t where t.contest.id = ?1")
     List<Team> findByContest(String contestId);
 
-    @Query("SELECT t FROM Team t WHERE t.contest_id IS NULL")
-    List<Team> findUnassigned();
-
 }

judgebean/src/main/resources/import.sql
@@ -41,3 +41,4 @@ insert into Resource (id, name, contest_id) values ('R8', 'T4 DNS', 'C2')
 insert into Resource (id, name, contest_id) values ('R9', 'T5 DNS', 'C2')
 insert into Resource (id, name, contest_id) values ('R10', 'T4 HTTP', 'C2')
 insert into Resource (id, name, contest_id) values ('R11', 'T5 HTTP', 'C2')
+insert into Resource (id, name) values ('R12', 'Extra HTTP')

judgebean/src/main/webapp/resources.xhtml
@@ -3,8 +3,8 @@
              xmlns:b="http://bootsfaces.net/ui">
 
     <!--Show the unassigned resources first-->
-    <ui:repeat var="_resource" value="#{unassignedResources}">
-        <b:panel look="warning" col-lg="6" title="#{_resource.name}" collapsed="#{true}"/>
+    <ui:repeat var="_ur" value="#{unassignedResources}">
+        <b:panel look="warning" col-lg="6" title="#{_ur.name}" collapsed="#{true}"/>
     </ui:repeat>
 
     <!--The remaining resources are grouped by contest-->

judgebean/src/main/webapp/teams.xhtml
@@ -2,9 +2,19 @@
              xmlns:ui="http://java.sun.com/jsf/facelets"
              xmlns:b="http://bootsfaces.net/ui">
 
-    <ui:repeat var="_team" value="#{teams}">
-        <b:panel col-lg="6" title="#{_team.name}" collapsed="#{true}">
+    <!--Unassigned teams first-->
+    <ui:repeat var="_team" value="#{unassignedTeams}">
+        <b:panel col-lg="6" title="#{_team.name}" look="warning" collapsed="#{true}">
             Flag: #{_team.flag}
         </b:panel>
+    </ui:repeat>
+
+    <!--Now print the rest of the teams, ordered by contest-->
+    <ui:repeat var="_contest" value="#{contests}">
+        <ui:repeat var="_team" value="#{_contest.teams}">
+            <b:panel col-lg="6" title="#{_contest.name}: #{_team.name}" collapsed="#{true}">
+                Flag: #{_team.flag}
+            </b:panel>
+        </ui:repeat>
     </ui:repeat>
 </ui:fragment>